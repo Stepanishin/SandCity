5b8a3b78d43f8b74decfd6e5573a3f373ece1283
Евгений Степанишин <stepanishin92@mail.ru>
2022-09-15 14:16:05 +0200

    add team profile name

--- src/components/pages/MainPage/Team/Team.tsx
+++ src/components/pages/MainPage/Team/Team.tsx
@@ -1,9 +1,9 @@
 import React, { FC } from 'react';
 import styles from './Team.module.css'
-import team1 from './img/Team1.png'
-import team2 from './img/Team2.png'
-import team3 from './img/Team3.png'
-import team4 from './img/Team4.png'
+import demi from './img/Demiurg.jpg'
+import nastya from './img/Nastya.jpg'
+import max from './img/Max.jpg'
+import zalowsski from './img/lazyAvatar.png'
 
 const Team:FC = () => {
 
@@ -14,45 +14,21 @@ const Team:FC = () => {
                 <h2 className={styles.Team_title} >TEAM</h2>
                 <div className={styles.Team_profile_container} >
 
-                    <div className={styles.Team_profile}>
-                        <img className={styles.Team_profile_avatar} src={team1} alt="avatar" />
-                        <p className={styles.Team_profile_name}>Max</p>
-                    </div>
+                    <a rel='noreferrer' target="_blank" href='https://twitter.com/crypto_demiurg' className={styles.Team_profile}>
+                        <img className={styles.Team_profile_avatar} src={demi} alt="avatar" />
+                    </a>
 
-                    <div className={styles.Team_profile}>
-                        <img className={styles.Team_profile_avatar} src={team2} alt="avatar" />
-                        <p className={styles.Team_profile_name}>Nastya</p>
-                    </div>
+                    <a rel='noreferrer' target="_blank" href='https://twitter.com/_nasteenish_' className={styles.Team_profile}>
+                        <img className={styles.Team_profile_avatar} src={nastya} alt="avatar" />
+                    </a>
 
-                    <div className={styles.Team_profile}>
-                        <img className={styles.Team_profile_avatar} src={team3} alt="avatar" />
-                        <p className={styles.Team_profile_name}>Max</p>
-                    </div>
+                    <a rel='noreferrer' target="_blank" href='https://twitter.com/lywyl_SOL' className={styles.Team_profile}>
+                        <img className={styles.Team_profile_avatar} src={max} alt="avatar" />
+                    </a>
 
-                    <div className={styles.Team_profile}>
-                        <img className={styles.Team_profile_avatar} src={team4} alt="avatar" />
-                        <p className={styles.Team_profile_name}>Max</p>
-                    </div>
-
-                    <div className={styles.Team_profile}>
-                        <img className={styles.Team_profile_avatar} src={team1} alt="avatar" />
-                        <p className={styles.Team_profile_name}>Max</p>
-                    </div>
-
-                    <div className={styles.Team_profile}>
-                        <img className={styles.Team_profile_avatar} src={team2} alt="avatar" />
-                        <p className={styles.Team_profile_name}>Max</p>
-                    </div>
-
-                    <div className={styles.Team_profile}>
-                        <img className={styles.Team_profile_avatar} src={team3} alt="avatar" />
-                        <p className={styles.Team_profile_name}>Max</p>
-                    </div>
-
-                    <div className={styles.Team_profile}>
-                        <img className={styles.Team_profile_avatar} src={team4} alt="avatar" />
-                        <p className={styles.Team_profile_name}>Max</p>
-                    </div>
+                    <a rel='noreferrer' target="_blank" href='https://twitter.com/Evgenii33952894' className={styles.Team_profile}>
+                        <img className={styles.Team_profile_avatar} src={zalowsski} alt="avatar" />
+                    </a>
 
                 </div>
             </div>
